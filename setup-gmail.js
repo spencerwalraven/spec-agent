d9b8d104fc5d490c69bb664b38c97249025542b7
Spencer Walraven <swalraven1@my.gcu.edu>
2026-03-24 23:46:35 -0700

    Add Drive and Docs scopes to OAuth setup

--- setup-gmail.js
+++ setup-gmail.js
@@ -20,6 +20,8 @@ const SCOPES = [
   'https://www.googleapis.com/auth/gmail.readonly',
   'https://www.googleapis.com/auth/gmail.modify',
   'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/documents',
 ];
 
 const authUrl = oauth2Client.generateAuthUrl({
